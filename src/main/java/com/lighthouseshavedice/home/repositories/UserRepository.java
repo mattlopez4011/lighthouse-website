773a439901fb79a9e80820d6373385631fb5bfe0
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-12-19 12:31:36 -0600

    added user with roles

--- src/main/java/com/lighthouseshavedice/home/repositories/UserRepository.java
+++ src/main/java/com/lighthouseshavedice/home/repositories/UserRepository.java
@@ -1,14 +1,1 @@
-package com.lighthouseshavedice.home.repositories;
 
-import com.lighthouseshavedice.home.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-    User findUserById(Long id);
-
-    List<User> findByUsernameContaining(String username);
-}
