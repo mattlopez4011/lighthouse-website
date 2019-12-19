1d327ea303efb6c8e08fc6b5ecaf552273fe5eea
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-12-19 12:34:12 -0600

    added line 11 into UserRepo

--- src/main/java/com/lighthouseshavedice/home/repositories/UserRepository.java
+++ src/main/java/com/lighthouseshavedice/home/repositories/UserRepository.java
@@ -1,1 +1,16 @@
+package com.lighthouseshavedice.home.repositories;
 
+import com.lighthouseshavedice.home.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //for spring security
+    User findByUsername(String username);
+
+    User findUserById(Long id);
+
+    List<User> findByUsernameContaining(String username);
+}
