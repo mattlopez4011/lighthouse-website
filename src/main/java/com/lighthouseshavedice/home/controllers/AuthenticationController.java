d8005104decb34eabc26366d06a8c584c0d1f6da
Matt Lopez <matthew.lopez4011@gmail.com>
2019-12-20 01:57:51 -0600

    add the controller annotation

--- src/main/java/com/lighthouseshavedice/home/controllers/AuthenticationController.java
+++ src/main/java/com/lighthouseshavedice/home/controllers/AuthenticationController.java
@@ -1,7 +1,9 @@
 package com.lighthouseshavedice.home.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class AuthenticationController {
 
     @GetMapping("/login")
