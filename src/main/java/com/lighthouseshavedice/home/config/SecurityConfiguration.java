83ddf401d97bc3fb56e4adb59852d7fcf1336898
Matt Lopez <matthew.lopez4011@gmail.com>
2019-12-20 18:51:57 -0600

    add grid, footer and navbar to index page

--- src/main/java/com/lighthouseshavedice/home/config/SecurityConfiguration.java
+++ src/main/java/com/lighthouseshavedice/home/config/SecurityConfiguration.java
@@ -46,12 +46,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/ads") // anyone can see the home and the ads pages
+                .antMatchers("/") // anyone can see the home and the ads pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
                 .authorizeRequests()
                 .antMatchers(
+                        "/dashboard",
                         "/ads/create", // only authenticated users can create ads
                         "/ads/{id}/edit" // only authenticated users can edit ads
                 )
