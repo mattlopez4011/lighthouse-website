1bd19358bffac4fe184c5f06c0dbf1355d882287
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-12-26 14:13:32 -0600

    got edit profile to show up

--- src/main/java/com/lighthouseshavedice/home/controllers/ProfileController.java
+++ src/main/java/com/lighthouseshavedice/home/controllers/ProfileController.java
@@ -1,10 +1,13 @@
 package com.lighthouseshavedice.home.controllers;
 
+import com.lighthouseshavedice.home.models.User;
 import com.lighthouseshavedice.home.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProfileController {
@@ -23,11 +26,23 @@ public class ProfileController {
         return "profile";
     }
 
-    @GetMapping("edit/{id}/profile")
+    @GetMapping("profile/{id}/edit")
     public String editProfile(Model model, @PathVariable long id){
         model.addAttribute("user", userDao.getOne(id));
 
         return "editProfile";
+    }
+
+    @PostMapping("profile/{id}/edit")
+    public String postProfile(@PathVariable long id, @RequestParam String name, @RequestParam String username){
+        User oldUserProfile = userDao.getOne(id);
+
+        oldUserProfile.setName(name);
+        oldUserProfile.setUsername(username);
+
+        userDao.save(oldUserProfile);
+
+        return "redirect:/profile/" +id;
     }
 
 }
