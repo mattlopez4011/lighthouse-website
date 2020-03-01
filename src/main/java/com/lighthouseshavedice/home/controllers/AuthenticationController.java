7543a13b10339dc380a55d4fd3d99c3116b4c428
Matt Lopez <matthew.lopez4011@gmail.com>
2020-03-01 08:41:54 -0800

    Fixed navbar icon image position

--- src/main/java/com/lighthouseshavedice/home/controllers/AuthenticationController.java
+++ src/main/java/com/lighthouseshavedice/home/controllers/AuthenticationController.java
@@ -2,7 +2,10 @@ package com.lighthouseshavedice.home.controllers;
 
 import com.lighthouseshavedice.home.models.User;
 import com.lighthouseshavedice.home.repositories.UserRepository;
+import org.hibernate.annotations.Parameter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +22,7 @@ public class AuthenticationController {
 
     @GetMapping("/login")
     public String showLoginForm() {
-        return "login";
+        return "index";
     }
 
     @PostMapping("/login")
@@ -29,10 +32,20 @@ public class AuthenticationController {
         try {
             u = userDao.findByUsername(username);
         } catch (NullPointerException e){
-            return "login";
+            return "index";
         }
 
         return "redirect:/dashboard/" + u.getId();
+    }
+
+    @GetMapping("/logout")
+    public String logout(Model model){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        long id = user.getId();
+//        model.addAttribute("users", userDao.getOne(id));/
+        model.addAttribute("isLoggedOut", true);
+//        model.addAttribute("smoke", smokeDao.getOne(id));
+        return "index";
     }
 
 }
