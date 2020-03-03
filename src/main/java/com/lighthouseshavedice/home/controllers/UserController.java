554ee133badf9f6271522ff1ee3ded380ce65046
Matt Lopez <matthew.lopez4011@gmail.com>
2020-03-03 08:15:30 -0800

    post to /register is working. :)

--- src/main/java/com/lighthouseshavedice/home/controllers/UserController.java
+++ src/main/java/com/lighthouseshavedice/home/controllers/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Date;
+
 @Controller
 public class UserController {
     private UserRepository userDao;
@@ -27,10 +29,7 @@ public class UserController {
 
     @PostMapping("/register")
     public String saveUser(@ModelAttribute User user){
-//        String hash = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(hash);
-//        userDao.save(user);
-//        return "redirect:/login";
+        Date d = new Date();
         System.out.println("==========================================");
         System.out.println("user.getName() = " + user.getName());
         System.out.println("user.getUsername() = " + user.getUsername());
@@ -39,8 +38,22 @@ public class UserController {
         System.out.println("user.getPhone_number() = " + user.getPhone_number());
         System.out.println("user.getGender() = " + user.getGender());
         System.out.println("user.getProfile_pic() = " + user.getProfile_pic());
+        System.out.println("user.getLast_login() = " + user.getLast_login());
+        System.out.println("Date " + d);
+
+
+
         System.out.println("THE END!!");
 
-        return "register";
+
+        //        String hash = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(hash);
+        user.setDate_created(d.toString());
+        user.setProfile_pic("https://cdn.filestackcontent.com/zubgVyqDTwK4ineQYCCp");
+        userDao.save(user);
+        return "redirect:/login?successNewUser";
+
+
+
     }
 }
