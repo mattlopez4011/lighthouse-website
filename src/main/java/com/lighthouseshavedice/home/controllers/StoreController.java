55da6efc7f115210ae355dcad36bf4c1c419744e
Matt Lopez <matthew.lopez4011@gmail.com>
2020-03-06 00:28:14 -0600

    start working on flavors table functionality

--- src/main/java/com/lighthouseshavedice/home/controllers/StoreController.java
+++ src/main/java/com/lighthouseshavedice/home/controllers/StoreController.java
@@ -1,7 +1,9 @@
 package com.lighthouseshavedice.home.controllers;
 
 import com.lighthouseshavedice.home.models.User;
+import com.lighthouseshavedice.home.repositories.FlavorRepository;
 import com.lighthouseshavedice.home.repositories.UserRepository;
+import com.lighthouseshavedice.home.util.UtilMethods;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StoreController {
     private final UserRepository userDao;
+    private final FlavorRepository flavorDao;
 
-    public StoreController(UserRepository userDao){
+    public StoreController(UserRepository userDao, FlavorRepository flavorDao){
+        this.flavorDao = flavorDao;
         this.userDao = userDao;
     }
 
@@ -21,7 +25,13 @@ public class StoreController {
         long id = user.getId();
         User signedInUser = userDao.getOne(id);
 
+//        Capitalize User name
+//        UtilMethods u = new UtilMethods();
+        String capUsername =  UtilMethods.capString(signedInUser.getName());
+
         model.addAttribute("user", signedInUser);
+        model.addAttribute("capName", capUsername);
+        model.addAttribute("flavor", flavorDao.findAll()); // Return list of all the flavors
 
         return "store-inventory-dashboard";
     }
